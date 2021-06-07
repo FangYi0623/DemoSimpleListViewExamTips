f5c8dfb0d4911f37f85401bdffc42870510c3ffb
FangYi0623 <20019648@myrp.edu.sg>
2021-06-07 10:32:13 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20019648/demosimplelistviewexamtips/MainActivity.java
@@ -0,0 +1,32 @@
+package sg.edu.rp.c346.id20019648.demosimplelistviewexamtips;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    ListView lvExamTips;
+    String tips[];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lvExamTips = findViewById(R.id.listViewExamTips);
+
+        tips = new String[5];
+
+        tips[0] = "Don't just read the code, code it as much as possible during each practical";
+        tips[1] = "Seek help from the lecturer ASAP, don't wait till you lost in the jungle";
+        tips[2] = "Prepare your template source code for each topic";
+        tips[3] = "Create a few empty Android projects to speed up your coding during the exam";
+        tips[4] = "Ensure that your Android Studio is up and running before the exam";
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tips);
+        // simple_list_item_1 --> display items as a simple TextView as layot for the items
+        lvExamTips.setAdapter(adapter);
+    }
+}
